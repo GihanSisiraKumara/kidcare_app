473623deac5a434562489729a487d9bb8a2c77d0
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-04-04 16:59:20 +0800

    add notification assets and design page

--- lib/screen/bellBord.dart
+++ lib/screen/bellBord.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kidcare/screen/dashBord.dart';
 
 class bellBord extends StatelessWidget {
   const bellBord({super.key, required String title});
@@ -16,9 +17,17 @@ class bellBord extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(height: 20),
-              const CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage('assets/images/kidcare.png'),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DashBord()),
+                  );
+                },
+                child: const CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('assets/images/ebell2.jpg'),
+                ),
               ),
               const SizedBox(height: 20),
               SizedBox(
@@ -38,30 +47,29 @@ class bellBord extends StatelessWidget {
                 CupertinoIcons.person,
               ),
               const SizedBox(height: 10),
-              itemProfile('Date', '2024/03/23', CupertinoIcons.calendar),
+              itemProfile('Date', '2024/03/23'),
               const SizedBox(height: 10),
-              itemProfile('Leave at Home', '07. 10 A.M.', CupertinoIcons.time),
+              itemProfile('Leave at Home', '07. 10 A.M.'),
               const SizedBox(height: 10),
-              itemProfile('Arival at Scool', '07.30 A.M', CupertinoIcons.time),
+              itemProfile('Arival at Scool', '07.30 A.M'),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
-              itemProfile(
-                  'Dispatcher at School', '13.30 P.M', CupertinoIcons.time),
+              itemProfile('Dispatcher at School', '13.30 P.M'),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
-              itemProfile('In Home', '14.30 P.M', CupertinoIcons.time),
+              itemProfile('In Home', '14.30 P.M'),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
-              itemProfile('In School', '07.30 A.M', CupertinoIcons.time),
+              itemProfile('In School', '07.30 A.M'),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
-              itemProfile('Out School', '13.35 P.M', CupertinoIcons.time),
+              itemProfile('Out School', '13.35 P.M'),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
             ],
           ),
@@ -70,23 +78,27 @@ class bellBord extends StatelessWidget {
     );
   }
 
-  itemProfile(String title, String subtitle, IconData iconData) {
+  itemProfile(String title, String subtitle, [IconData? iconData]) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
+          color: const Color.fromARGB(255, 232, 188, 188),
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
                 offset: const Offset(0, 5),
-                color: Colors.deepOrange.withOpacity(.2),
+                color: const Color.fromARGB(255, 246, 244, 244).withOpacity(.2),
                 spreadRadius: 2,
                 blurRadius: 10)
           ]),
       child: ListTile(
         title: Text(title),
         subtitle: Text(subtitle),
-        leading: Icon(iconData),
-        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
+        leading: const CircleAvatar(
+          radius: 25,
+          backgroundImage: AssetImage("assets/images/q.webp"),
+        ),
+        trailing: const Icon(Icons.arrow_forward,
+            color: Color.fromARGB(255, 17, 13, 13)),
         tileColor: Colors.white,
       ),
     );
