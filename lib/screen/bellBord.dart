f7707d7302a9d4b0e01d4a01ce0eb3cfbe343fda
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-04-07 20:35:11 +0800

    edit page

--- lib/screen/bellBord.dart
+++ lib/screen/bellBord.dart
@@ -30,68 +30,82 @@ class bellBord extends StatelessWidget {
         ],
         title: const Text("Emergency Bell!"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const DashBord()),
-                  );
-                },
-                child: const CircleAvatar(
-                  radius: 70,
-                  backgroundImage: AssetImage('assets/images/ebell2.jpg'),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/red2.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DashBord()),
+                    );
+                  },
+                  child: const CircleAvatar(
+                    radius: 70,
+                    backgroundImage: AssetImage('assets/images/ebell2.jpg'),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 130,
-                height: 50,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.all(15),
-                    ),
-                    child: const Text('Deseble Bell')),
-              ),
-              const SizedBox(height: 20),
-              itemProfile(
-                'Name                             1m',
-                'Ahad Hashmi',
-                CupertinoIcons.person,
-              ),
-              const SizedBox(height: 10),
-              itemProfile(
-                  'Date                               2m', '2024/03/23'),
-              const SizedBox(height: 10),
-              itemProfile('Leave at Home              5h', '07. 10 A.M.'),
-              const SizedBox(height: 10),
-              itemProfile('Arival at Scool                8h ', '07.30 A.M'),
-              const SizedBox(
-                height: 10,
-              ),
-              itemProfile('Dispatcher at School      1d', '13.30 P.M'),
-              const SizedBox(
-                height: 10,
-              ),
-              itemProfile('In Home                           1d', '14.30 P.M'),
-              const SizedBox(
-                height: 10,
-              ),
-              itemProfile('In School                         2d', '07.30 A.M'),
-              const SizedBox(
-                height: 10,
-              ),
-              itemProfile('Out School                       7d', '13.35 P.M'),
-              const SizedBox(
-                height: 10,
-              ),
-            ],
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 130,
+                  height: 50,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(15),
+                      ),
+                      child: const Text('Deseble Bell')),
+                ),
+                const SizedBox(height: 20),
+                itemProfile(
+                  'Name                                       1m',
+                  'Ahad Hashmi',
+                  CupertinoIcons.person,
+                ),
+                const SizedBox(height: 10),
+                itemProfile('Date                                          2m',
+                    '2024/03/23'),
+                const SizedBox(height: 10),
+                itemProfile(
+                    'Leave at Home                         5h', '07. 10 A.M.'),
+                const SizedBox(height: 10),
+                itemProfile('Arival at Scool                          8h ',
+                    '07.30 A.M'),
+                const SizedBox(
+                  height: 10,
+                ),
+                itemProfile(
+                    'Dispatcher at School               1d', '13.30 P.M'),
+                const SizedBox(
+                  height: 10,
+                ),
+                itemProfile('In Home                                    1d',
+                    '14.30 P.M'),
+                const SizedBox(
+                  height: 10,
+                ),
+                itemProfile('In School                                   2d',
+                    '07.30 A.M'),
+                const SizedBox(
+                  height: 10,
+                ),
+                itemProfile('Out School                                7d',
+                    '13.35 P.M'),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -101,7 +115,7 @@ class bellBord extends StatelessWidget {
   itemProfile(String title, String subtitle, [IconData? iconData]) {
     return Container(
       decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 232, 188, 188),
+          color: const Color.fromARGB(255, 249, 244, 244),
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
@@ -117,9 +131,9 @@ class bellBord extends StatelessWidget {
           radius: 25,
           backgroundImage: AssetImage("assets/images/q.webp"),
         ),
-        trailing: const Icon(Icons.arrow_forward,
-            color: Color.fromARGB(255, 17, 13, 13)),
-        tileColor: Colors.white,
+        // trailing: const Icon(Icons.arrow_forward,
+        // color: Color.fromARGB(255, 17, 13, 13)),
+        // tileColor: Colors.white,
       ),
     );
   }
--- lib/screen/viewBord.dart
+++ lib/screen/viewBord.dart
@@ -29,91 +29,101 @@ class viewBord extends StatelessWidget {
         ],
         title: const Text('Sheduling View'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              const CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage('assets/images/kidcare.png'),
-              ),
-              const SizedBox(height: 20),
-              itemProfile(
-                'Name',
-                'Ahad Hashmi',
-                CupertinoIcons.person,
-              ),
-              const SizedBox(height: 10),
-              itemProfile('Date', '2024/03/23', CupertinoIcons.calendar),
-              const SizedBox(height: 10),
-              itemProfile('Leave at Home', '07. 10 A.M.', CupertinoIcons.time),
-              const SizedBox(height: 10),
-              itemProfile('Arival at Scool', '07.30 A.M', CupertinoIcons.time),
-              const SizedBox(
-                height: 10,
-              ),
-              itemProfile(
-                  'Dispatcher at School', '13.30 P.M', CupertinoIcons.time),
-              const SizedBox(
-                height: 10,
-              ),
-              itemProfile('In Home', '14.30 P.M', CupertinoIcons.time),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "Attendance",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 190, 88, 88),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/red2.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                const CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('assets/images/kidcare.png'),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              itemProfile('In School', '07.30 A.M', CupertinoIcons.time),
-              const SizedBox(
-                height: 20,
-              ),
-              itemProfile('Out School', '13.35 P.M', CupertinoIcons.time),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [
-                      Color(0xffB81736),
-                      Color(0xff281537),
-                    ],
+                const SizedBox(height: 20),
+                itemProfile(
+                  'Name',
+                  'Ahad Hashmi',
+                  CupertinoIcons.person,
+                ),
+                const SizedBox(height: 10),
+                itemProfile('Date', '2024/03/23', CupertinoIcons.calendar),
+                const SizedBox(height: 10),
+                itemProfile(
+                    'Leave at Home', '07. 10 A.M.', CupertinoIcons.time),
+                const SizedBox(height: 10),
+                itemProfile(
+                    'Arival at Scool', '07.30 A.M', CupertinoIcons.time),
+                const SizedBox(
+                  height: 10,
+                ),
+                itemProfile(
+                    'Dispatcher at School', '13.30 P.M', CupertinoIcons.time),
+                const SizedBox(
+                  height: 10,
+                ),
+                itemProfile('In Home', '14.30 P.M', CupertinoIcons.time),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Attendance",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 190, 88, 88),
                   ),
-                  borderRadius: BorderRadius.circular(10),
                 ),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(15),
-                    backgroundColor:
-                        Colors.transparent, // Set primary color to transparent
-                    shadowColor: Colors.transparent, // Hide shadow
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                const SizedBox(
+                  height: 20,
+                ),
+                itemProfile('In School', '07.30 A.M', CupertinoIcons.time),
+                const SizedBox(
+                  height: 20,
+                ),
+                itemProfile('Out School', '13.35 P.M', CupertinoIcons.time),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color(0xffB81736),
+                        Color(0xff281537),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(15),
+                      backgroundColor: Colors
+                          .transparent, // Set primary color to transparent
+                      shadowColor: Colors.transparent, // Hide shadow
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      'Edit Shedule',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                  child: const Text(
-                    'Edit Profile',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -123,7 +133,7 @@ class viewBord extends StatelessWidget {
   itemProfile(String title, String subtitle, IconData iconData) {
     return Container(
       decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 240, 192, 192),
+          color: const Color.fromARGB(255, 248, 238, 238),
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
