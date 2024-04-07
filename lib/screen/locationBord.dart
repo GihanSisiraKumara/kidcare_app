28d08b3fd35c70495d33b4d57dbe7e53627dfe7c
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-04-07 22:31:45 +0800

    add share locatio fetures

--- lib/screen/locationBord.dart
+++ lib/screen/locationBord.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:share/share.dart';
 
 class LocationBoard extends StatefulWidget {
   const LocationBoard({Key? key, required this.title}) : super(key: key);
@@ -17,46 +15,83 @@ class LocationBoard extends StatefulWidget {
 class _LocationBoardState extends State<LocationBoard> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
-
+  final Set<Marker> _markers = <Marker>{};
+  final Set<Polygon> _polygons = <Polygon>{};
+  final List<LatLng> _polygonLatLongs = <LatLng>[];
+  int _polygonIdCounter = 1;
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(6.9271, 79.8612), // Colombo coordinates
-    zoom: 14.0, // You can adjust the zoom level as needed
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
   );
+
+  @override
+  void initState() {
+    super.initState();
+    _setMarker(const LatLng(37.42796133580664, -122.085749655962));
+  }
+
+  void _setMarker(LatLng latLng) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: const MarkerId('id-1'),
+          position: latLng,
+        ),
+      );
+    });
+  }
+
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_id_$_polygonIdCounter';
+    _polygonIdCounter++;
+    _polygons.add(
+      Polygon(
+          polygonId: PolygonId(polygonIdVal),
+          points: _polygonLatLongs,
+          strokeWidth: 2,
+          fillColor: Colors.transparent),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text('Share your location'),
+        backgroundColor: const Color.fromARGB(255, 193, 211, 226),
+        title: const Text('Share Your Location'),
       ),
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Colors.blue,
-        onPressed: _currentLocation,
-        label: const Text('Share your location!'),
-        icon: const Icon(Icons.person),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+                mapType: MapType.satellite,
+                markers: _markers,
+                polylines: Set<Polyline>.from(_polygons),
+                initialCameraPosition: _kGooglePlex,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+                onTap: (point) {
+                  _polygonLatLongs.add(point);
+                  _setPolygon();
+                }),
+          ),
+        ],
       ),
     );
   }
 
-  Future<void> _currentLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-
-      final String message =
-          'Check out my current location: https://www.google.com/maps/search/?api=1&query=${position.latitude},${position.longitude}';
-
-      Share.share(message);
-    } catch (e) {
-      // Handle errors
-      print('Error getting current location: $e');
-    }
+  Future<void> _goToPlace(Map<String, dynamic> place) async {
+    final double lat = place['geometry']['location']['lat'];
+    final double lng = place['geometry']['location']['lng'];
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(lat, lng),
+        zoom: 12,
+      ),
+    ));
+    _setMarker(LatLng(lat, lng));
   }
 }
