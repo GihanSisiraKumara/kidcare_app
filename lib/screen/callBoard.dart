7d472f35be8f16795436911994bf8bb5a7e72d3c
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-04-07 15:29:01 +0800

    edit design

--- lib/screen/callBoard.dart
+++ lib/screen/callBoard.dart
@@ -50,82 +50,96 @@ class _CallBoardState extends State<CallBoard> {
         ],
         title: const Text('Call'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.separated(
-              itemBuilder: (context, index) {
-                return Slidable(
-                  endActionPane: ActionPane(
-                    motion: const ScrollMotion(),
-                    children: [
-                      SlidableAction(
-                        onPressed: (context) {},
-                        icon: Icons.reply,
-                        backgroundColor: Colors.grey[300]!,
-                      ),
-                      SlidableAction(
-                        onPressed: (context) {},
-                        icon: Icons.delete,
-                        foregroundColor: Colors.white,
-                        backgroundColor: const Color.fromARGB(255, 238, 28, 28),
-                      )
-                    ],
-                  ),
-                  child: ListTile(
-                    isThreeLine:
-                        true, //get more space in eact nortification container.
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: size.width * 0.08),
-                    leading: const CircleAvatar(
-                      radius: 25,
-                      backgroundImage: AssetImage("assets/images/call.avif"),
-                    ),
-                    title: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Good afternoon",
-                          style: TextStyle(fontWeight: FontWeight.w600),
-                        ),
-                        Text(
-                          "2h Ago",
-                          style: TextStyle(fontSize: 12, color: Colors.black),
-                        )
-                      ],
-                    ),
-                    subtitle: const Text(
-                      "plase fill all the text feild and conform that",
-                      style: TextStyle(color: Color.fromARGB(255, 11, 11, 11)),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                );
-              },
-              separatorBuilder: ((context, index) => Divider(
-                    color: const Color.fromARGB(255, 200, 178, 178),
-                    indent: size.width * .08,
-                    endIndent: size.width * .08,
-                  )),
-              itemCount: 5,
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/red.jpg'),
+            fit: BoxFit.cover,
           ),
-          GestureDetector(
-            onTap: callNumber,
-            child: const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Center(
-                child: SizedBox(
-                  child: CircleAvatar(
-                    radius: 70,
-                    backgroundImage: AssetImage('assets/images/call1.jpg'),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Slidable(
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            onPressed: (context) {},
+                            icon: Icons.reply,
+                            backgroundColor: Colors.grey[300]!,
+                          ),
+                          SlidableAction(
+                            onPressed: (context) {},
+                            icon: Icons.delete,
+                            foregroundColor: Colors.white,
+                            backgroundColor:
+                                const Color.fromARGB(255, 238, 28, 28),
+                          )
+                        ],
+                      ),
+                      child: ListTile(
+                        isThreeLine:
+                            false, //get more space in eact nortification container.
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: size.width * 0.08),
+                        leading: const CircleAvatar(
+                          radius: 25,
+                          backgroundImage: AssetImage("assets/images/q.webp"),
+                        ),
+                        title: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Good afternoon",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              "2h Ago",
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.black),
+                            )
+                          ],
+                        ),
+                        subtitle: const Text(
+                          "plase fill all the text feild and conform that",
+                          style:
+                              TextStyle(color: Color.fromARGB(255, 11, 11, 11)),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: ((context, index) => Divider(
+                        color: const Color.fromARGB(255, 200, 178, 178),
+                        indent: size.width * .08,
+                        endIndent: size.width * .08,
+                      )),
+                  itemCount: 5,
+                ),
+              ),
+              GestureDetector(
+                onTap: callNumber,
+                child: const Padding(
+                  padding: EdgeInsets.all(50.0),
+                  child: Center(
+                    child: SizedBox(
+                      child: CircleAvatar(
+                        radius: 67,
+                        backgroundImage: AssetImage('assets/images/call.avif'),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
