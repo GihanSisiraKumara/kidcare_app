87ccc9d80bc13a7d1e33623893d26b7bd32c7c6a
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-05-05 23:53:06 +0800

    solving page error

--- lib/screen2/parentnotifiBord.dart
+++ lib/screen2/parentnotifiBord.dart
@@ -1,117 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-
 class parentnotifiBord extends StatelessWidget {
   const parentnotifiBord({super.key, required String title});
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromARGB(255, 241, 178, 173),
-appBar: AppBar(
-  centerTitle: true,
-  leading: IconButton(
-    style: ButtonStyle(
-      iconSize: MaterialStateProperty.all<double>(30),
-      iconColor: MaterialStateProperty.all<Color>(
-          const Color.fromARGB(255, 252, 251, 251)),
-      backgroundColor: MaterialStateProperty.all<Color>(
-          const Color.fromARGB(255, 234, 117, 117)),
-    ),
-    icon: const Icon(Icons.arrow_back),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  ),
-  title: const Text(
-    "Notification",
-    style: TextStyle(
-        fontSize: 22,
-        color: Color.fromARGB(255, 247, 84, 84),
-        fontWeight: FontWeight.bold),
-  ),
-),
-body: Column(
-  children: [
-    Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: TextField(
-        decoration: InputDecoration(
-            prefixIcon: const Icon(Icons.search),
-            hintText: "Search notification",
-            contentPadding: const EdgeInsets.all(16.0),
-            fillColor: Colors.black12,
-            filled: true,
-            border: OutlineInputBorder(
-                borderSide: BorderSide.none,
-                borderRadius: BorderRadius.circular(20.0))),
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          style: ButtonStyle(
+            iconSize: MaterialStateProperty.all<double>(30),
+            iconColor: MaterialStateProperty.all<Color>(
+                const Color.fromARGB(255, 252, 251, 251)),
+            backgroundColor: MaterialStateProperty.all<Color>(
+                const Color.fromARGB(255, 234, 117, 117)),
+          ),
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          "Notification",
+          style: TextStyle(
+              fontSize: 22,
+              color: Color.fromARGB(255, 247, 84, 84),
+              fontWeight: FontWeight.bold),
+        ),
       ),
-    ),
-    const SizedBox(height: 10),
-    Expanded(
-      child: ListView.separated(
-        itemBuilder: (context, index) {
-          return Slidable(
-            endActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              children: [
-                SlidableAction(
-                  onPressed: (context) {},
-                  icon: Icons.reply,
-                  backgroundColor: Colors.grey[300]!,
-                ),
-                SlidableAction(
-                  onPressed: (context) {},
-                  icon: Icons.delete,
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color.fromARGB(255, 238, 28, 28),
-                )
-              ],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: "Search notification",
+                  contentPadding: const EdgeInsets.all(16.0),
+                  fillColor: Colors.black12,
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(20.0))),
             ),
-            child: ListTile(
-              isThreeLine:
-                  true, //get more space in eact nortification container.
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: size.width * 0.08),
-              leading: const CircleAvatar(
-                radius: 25,
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage("assets/images/logo.png"),
-              ),
-              title: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Kidcare alert!",
-                    style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return Slidable(
+                  endActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {},
+                        icon: Icons.reply,
+                        backgroundColor: Colors.grey[300]!,
+                      ),
+                      SlidableAction(
+                        onPressed: (context) {},
+                        icon: Icons.delete,
+                        foregroundColor: Colors.white,
+                        backgroundColor: const Color.fromARGB(255, 238, 28, 28),
+                      )
+                    ],
                   ),
-                  Text(
-                    "2h Ago",
-                    style: TextStyle(fontSize: 12, color: Colors.black),
-                  )
-                ],
-              ),
-              subtitle: const Text(
-                "Your child is not feeling well, please check him/her. please check shedules for more details.",
-                style: TextStyle(color: Color.fromARGB(255, 11, 11, 11)),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
+                  child: ListTile(
+                    isThreeLine:
+                        true, //get more space in eact nortification container.
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: size.width * 0.08),
+                    leading: const CircleAvatar(
+                      radius: 25,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage("assets/images/logo.png"),
+                    ),
+                    title: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Kidcare alert!",
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "2h Ago",
+                          style: TextStyle(fontSize: 12, color: Colors.black),
+                        )
+                      ],
+                    ),
+                    subtitle: const Text(
+                      "Your child is not feeling well, please check him/her. please check shedules for more details.",
+                      style: TextStyle(color: Color.fromARGB(255, 11, 11, 11)),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: ((context, index) => Divider(
+                    color: const Color.fromARGB(255, 200, 178, 178),
+                    indent: size.width * .08,
+                    endIndent: size.width * .08,
+                  )),
+              itemCount: 20,
             ),
-          );
-        },
-        separatorBuilder: ((context, index) => Divider(
-              color: const Color.fromARGB(255, 200, 178, 178),
-              indent: size.width * .08,
-              endIndent: size.width * .08,
-            )),
-        itemCount: 20,
+          ),
+        ],
       ),
-    ),
-  ],
-),
     );
   }
 }
